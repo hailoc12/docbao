f408a45dc1c238c5042085815a6a8b8bbdb2d03e
hailoc12 <hailoc12@gmail.com>
2018-10-16 23:28:11 +0700

    update version 1.9.2

--- run_docbao.sh
+++ run_docbao.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 PATH=/home/pi/miniconda3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
-echo "start running docbao_congnghe"
-cd ~/docbao_congnghe/backend/category
+echo "start running docbao_vietanh"
+cd ~/docbao_vietanh/backend/category
 rm *
-cd ~/docbao_congnghe/backend
-rclone sync -v docbao_congnghe:category ~/docbao_congnghe/backend/category
-rclone copy -v docbao_congnghe:output ~/docbao_congnghe/backend/input
-python3 ~/docbao_congnghe/backend/docbao.py
-python3 ~/docbao_congnghe/backend/event_detect.py 
-rclone delete docbao_congnghe:export/log_data.json
-rclone copy -v ~/docbao_congnghe/backend/export docbao_congnghe:export	
-echo "stop docbao_congnghe"
+cd ~/docbao_vietanh/backend
+rclone sync -v docbao_vietanh:category ~/docbao_vietanh/backend/category
+rclone copy -v docbao_vietanh:output ~/docbao_vietanh/backend/input
+python3 ~/docbao_vietanh/backend/docbao.py
+python3 ~/docbao_vietanh/backend/event_detect.py 
+rclone delete docbao_vietanh:export/log_data.json
+rclone copy -v ~/docbao_vietanh/backend/export docbao_vietanh:export	
+echo "stop docbao_vietanh"
 
