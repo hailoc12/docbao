c908d1259867df145949f63728c3c1d666406e07
hailoc12 <hailoc12@gmail.com>
2018-12-25 19:46:30 +0000

    update install on raspberry

--- install_on_raspberry3.sh
+++ install_on_raspberry3.sh
@@ -9,15 +9,15 @@ echo "Step 2: install firefox and xvfb"
 sudo apt install iceweasel
 sudo apt install xvfb
 echo "Step 3: copy geckodriver to /usr/bin"
-sudo cp ./resources/firefox-driver/raspberry3/geckodriver /usr/bin/
-sudo cp ./resources/firefox-driver/raspberry3/crawl.py ./backend/lib/
+sudo cp ./resources/firefox_driver/raspberry3/geckodriver /usr/bin/
+sudo cp ./resources/firefox_driver/raspberry3/crawl.py ./backend/lib/
 echo "Step 4: install rclone"
 curl https://rclone.org/install.sh | sudo bash
 echo "Step 5: config remoate ftp host in rclone as 'docbao'"
 echo "REMEMBER: use remote hostname as 'docbao'"
 rclone config
 echo "Try to push frontend to remote host"
-rclone copy -v ./frontend/* docbao:
+rclone sync -v ./frontend/ docbao:
 echo "Try to list remote host"
 rclone ls docbao:
 echo "Step 6: run test"
