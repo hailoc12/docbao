4ce41bf40e524470e71fc150ad42764aa85118be
hailoc12 <hailoc12@gmail.com>
2018-12-25 21:42:47 +0700

    add geckodriver

--- setup_linux.sh
+++ setup_linux.sh
@@ -8,13 +8,15 @@ if [$? -eq 0 ]; then
 else
 	sudo apt install firefox-esr
 fi
-echo "Step 3: install rclone"
+echo "Step 3: copy geckodriver to /usr/bin"
+cp ./resources/geckodriver /usr/bin/
+echo "Step 4: install rclone"
 curl https://rclone.org/install.sh | sudo bash
-echo "Step 4: config remoate ftp host in rclone as 'docbao'"
+echo "Step 5: config remoate ftp host in rclone as 'docbao'"
 echo "REMEMBER: use remote hostname as 'docbao'"
 rclone config
 echo "Try to push frontend to remote host"
 rclone copy -v ./frontend/* docbao:
-echo "Step 5: run test"
+echo "Step 6: run test"
 python3 ./backend/test.py
 echo "Finish !"
