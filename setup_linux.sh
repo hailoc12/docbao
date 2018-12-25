55a996f0dc6520e6333de7423701f693fd5a2aeb
hailoc12 <hailoc12@gmail.com>
2018-12-25 14:24:01 +0000

    update setup_linux.sh

--- setup_linux.sh
+++ setup_linux.sh
@@ -3,11 +3,18 @@ echo "Step 1: install python libraries"
 pip3 install -r requirements.txt
 echo "Step 2: install firefox"
 sudo apt install firefox
+if [$? -eq 0 ]; then
+	echo OK
+else
+	sudo apt install firefox-esr
+fi
 echo "Step 3: install rclone"
 curl https://rclone.org/install.sh | sudo bash
-echo "Step 4: config ftp host in rclone as 'docbao'"
-echo "REMEMBER: use host name as 'docbao'"
+echo "Step 4: config remoate ftp host in rclone as 'docbao'"
+echo "REMEMBER: use remote hostname as 'docbao'"
 rclone config
+echo "Try to push frontend to remote host"
+rclone copy -v ./frontend/* docbao:
 echo "Step 5: run test"
 python3 ./backend/test.py
 echo "Finish !"
