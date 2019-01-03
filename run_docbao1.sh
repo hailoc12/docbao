21a258dc9cec0ac5acf42c346b7f2968a1da61c0
hailoc12 <hailoc12@gmail.com>
2019-01-03 10:24:08 +0000

    1.0.1

--- run_docbao1.sh
+++ run_docbao1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export local_dir=docbao
-export remote_dir=vietnam
+export local_dir="docbao"
+export remote_dir="vietnam"
 PATH=/home/pi/miniconda3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
 echo "start running docbao"
 cd ~/$local_dir/backend/category
@@ -8,8 +8,6 @@ cd ~/$local_dir/backend/category
 cd ~/$local_dir/backend
 rclone sync -v $remote_dir:category ~/$local_dir/backend/category
 rclone copy -v $remote_dir:output ~/$local_dir/backend/input
-python3 ~/$local_dir/backend/docbao.py
-python3 ~/$local_dir/backend/event_detect.py 
 rclone delete $remote_dir:export/log_data.json
 rclone copy -v ~/$local_dir/backend/export $remote_dir:export	
 echo "stop docbao"
