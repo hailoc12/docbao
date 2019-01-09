3b25d70cbf7cd3dab6f96011e02c731129739329
hailoc12 <hailoc12@gmail.com>
2019-01-09 03:31:39 +0000

    1.0.0

--- backend/lib/utils.py
+++ backend/lib/utils.py
@@ -5,7 +5,6 @@ import codecs
 from datetime import datetime
 import os
 import urllib.request
-from fake_useragent import UserAgent
 from  lib.crawl import *
 import time
 
@@ -60,13 +59,13 @@ def open_binary_file_to_read(filename):
 def read_url_source_as_soup(url, use_browser=False, timeout=5):  # return page as soup of BeautifulSoup
     global _firefox_browser
 
-    f_hdr = {
-        'User-Agent': UserAgent().chrome,
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        'Accept-Encoding': 'none',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Connection': 'keep-alive'}
+    #f_hdr = {
+    #    'User-Agent': UserAgent().chrome,
+    #    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    #    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    #    'Accept-Encoding': 'none',
+    #    'Accept-Language': 'en-US,en;q=0.8',
+    #    'Connection': 'keep-alive'}
 
     hdr = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
