334ba14542cac888f1bb560a80fd425f4a07ccae
hailoc12 <hailoc12@gmail.com>
2019-04-04 00:46:25 +0700

    1.3.0

--- backend/lib/utils.py
+++ backend/lib/utils.py
@@ -86,8 +86,8 @@ def read_url_source_as_soup(url, use_browser=False, _display_browser=False, _fir
     a=True
     result = False
     browser = None
-    while a:
-    #try:
+    #while a:
+    try:
         print(url)
         html_source = None
         if use_browser == False:
@@ -131,9 +131,9 @@ def read_url_source_as_soup(url, use_browser=False, _display_browser=False, _fir
             return BeautifulSoup(html_source,features="html.parser")
         else:
             return None
-    #except:
-    #    print("Khong the mo trang: " + url)
-    #    return None
+    except:
+        print("Khong the mo trang: " + url)
+        return None
 
 def quit_browser():
     global _firefox_browser
