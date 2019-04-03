34ec9489ee56f5a145b30ad42adffdc965c4bf48
hailoc12 <hailoc12@gmail.com>
2019-04-03 22:48:40 +0700

    1.3.0

--- backend/lib/__init__.py
+++
@@ -1,5 +0,0 @@
-from lib.utils import *
-from lib.config import *
-from lib.data import *
-from lib.keyword import *
-from lib.category import *
--- resources/firefox_driver/raspberry3/note.md
+++
@@ -1,4 +0,0 @@
-Geckodriver 0.17
-Iceweasel (Firefox 52)
-Selenium 1.14
-
